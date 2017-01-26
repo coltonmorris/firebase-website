1d2254c86f9665a09b0e5d2095ba65f477a21aa3
Colton Morris <coltonmorris94@gmail.com>
2017-01-25 18:05:42 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,85 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import * as firebase from 'firebase'
+       
+const config = {  
+  apiKey: "AIzaSyCx06JqsDagobs82siVr5xQVvB0_YHxQzI",
+  authDomain: "fir-7cd9c.firebaseapp.com",
+  databaseURL: "https://fir-7cd9c.firebaseio.com",
+  storageBucket: "fir-7cd9c.appspot.com",
+}
+firebase.initializeApp(config)
+
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      Leaders: [],
+      email: 'email',
+      score: 1,
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.addToDb = this.addToDb.bind(this)
+  }
+
+  addToDb() {
+    let num = Math.floor((Math.random() * 10))
+    firebase.app().database().ref('Leaders/' + num).set({
+      email: this.state.email,
+      score: parseInt(this.state.score),
+    })
+  }
+
+  componentDidMount() {
+    let ref = firebase.app().database().ref().child('Leaders')
+       
+    ref.on('value', snap => {
+      this.setState({
+        Leaders: snap.val()
+      })
+    })
+  }
+
+  handleChange(event) {
+    const target = event.target
+    const value = target.value
+    const name = target.name
+
+    this.setState({
+      [name]: value
+    })
+  }
+
   render() {
+    // this.state.Leaders.sort((a,b) => {
+    //   return b.score - a.score
+    // })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <form>
+          <input onChange={this.handleChange} value={this.state.email} type="text" name="email" />
+          <input onChange={this.handleChange} value={this.state.score} type="text" name="score" />
+          <input type="button" onClick={this.addToDb} value="Submit" />
+        </form>
       </div>
-    );
+      <table>
+      <tbody>
+        {this.state.Leaders.map((player) => {
+          return (<tr key={player.email+player.score}> 
+              <td>{player.email}</td>
+              <td>{player.score}</td>
+              </tr>)
+        })}
+      </tbody>
+      </table>
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,56 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import * as firebase from 'firebase'
-// import App from './App'
+import App from './App'
 import './index.css'
 
-
-const config = {  
-  apiKey: "AIzaSyCx06JqsDagobs82siVr5xQVvB0_YHxQzI",
-  authDomain: "fir-7cd9c.firebaseapp.com",
-  databaseURL: "https://fir-7cd9c.firebaseio.com",
-  storageBucket: "fir-7cd9c.appspot.com",
-  messagingSenderId: "52778606417"
-}
-
-const database = firebase  
-  .initializeApp(config)
-  .database()
-  .ref()
-
-// Added some "action" functions
-// These will update our firebase database
-const addLocation = data => database.child('locations').push(data, response => response);  
-const updateLocation = (id, data) => database.child(`locations/${id}`).update(data, response => response);  
-const actions = {  
-  addLocation,
-  updateLocation,
-};
-
-// Now we can use our actions in our components
-const App = (props) => {  
-  console.log('snapshot', props);
-  return (
-    <div>
-      <h1>My Prototype</h1>
-      <button
-        onClick={() => props.addLocation({ name: 'China', region: 'Asia' })}
-      >
-        Add Stuff
-      </button>
-      <p>{JSON.stringify(props.location)}</p>
-    </div>
-  );
-}
-
-// Make sure you pass your actions into your root component
-database.on('value', snapshot => {  
-  const store = snapshot.val();
-  ReactDOM.render(
-    <App
-      {...actions}
-      {...store}
-    />,
-    document.getElementById('root')
-  )
-});
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
